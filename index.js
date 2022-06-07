ddc25d1b9aafbd24a3ed56eab6c5f4dae14a28f8
Christopher Babb <chris@appsoftdev.com>
2022-06-07 11:30:53 -0700

    commit tree 0443d32d16d122e49d1a31b5a137843bb5252ea6

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(value){
+    if(value >= 42){
+        return value - 42
+    }
+    else{
+        return 42 - value
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    if(start >= destination){
+        return (start - destination) * 264
+    }
+    else{
+        return (destination - start) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    let distanceTraveling = distanceTravelledInFeet(start, destination)
+    if(distanceTraveling < 400){
+        return 0
+    }
+    else if(distanceTraveling <= 2000){
+        return (distanceTraveling - 400) * 0.02
+    }
+    else if(distanceTraveling <= 2500){
+        return 25
+    }
+    else{
+        return 'cannot travel that far'
+    }
+}
